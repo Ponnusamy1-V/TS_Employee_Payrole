b0993b5e7cc545a51b25295011fbb33c7b5f1586
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2021-12-17 12:31:14 +0530

    Query report Filter

---
+++ hotel_management/hotel_management/report/ts_paymentslip/ts_paymentslip.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, ponnusamy and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["TS_PaymentSlip"] = {
+	"filters": [
+
+	]
+};
---
+++ hotel_management/hotel_management/report/ts_paymentslip/ts_paymentslip.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2013, ponnusamy and contributors
+# For license information, please see license.txt
+
+# import frappe
+# get_data get_column
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
